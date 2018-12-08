601cb7756bb1e821b2976a2a0e8798e3dbfd5666
Chris Karr <cpkarr@me.com>
2018-12-07 17:27:29 -0800

    Initial Commit

--- MySync.go
+++ MySync.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	var path string
+	var localErr error
+
+	path, localErr = exec.LookPath("rsync")
+
+	if localErr != nil {
+		fmt.Println("Cannot find rsync")
+		os.Exit(404)
+	}
+
+	localCmd := exec.Command(path, "-a /Users/ckarr/go/src /Users/ckarr/Destination/")
+	localErr = localCmd.Run()
+	if localErr == nil {
+		fmt.Println("rsync called successfully")
+	} else {
+		fmt.Println("rsync called unuccessfully")
+	}
 }
